30159d5a9712bb22f8098ae3a571fe053aa68c10
Hamza Langah <mh2094463@gmail.com>
2025-04-09 11:12:44 +0500

    full

---
+++ Assets/AutoTypeText/AutoTypeText.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AutoTypeText : MonoBehaviour
+{
+
+    public float letterPause = 0.2f;
+    public AudioClip sound;
+    private string word;
+    public bool music = true;
+
+    private Text uiText;
+    private AudioSource audioSource;
+
+    public void OnEnable()
+    {
+        uiText = GetComponent<Text>();
+        audioSource = GetComponent<AudioSource>();
+
+        if (uiText != null)
+        {
+            word = uiText.text;
+            uiText.text = "";  // Start with an empty text
+            StartCoroutine(TypeText());
+        }
+        else
+        {
+            Debug.LogError("Text component not found!");
+        }
+    }
+
+    public void OnDisable()
+    {
+        if (uiText != null)
+        {
+            uiText.text = "";  // Clear text on disable
+        }
+    }
+
+    public IEnumerator TypeText()
+    {
+        if (music)
+        {
+            foreach (char letter in word.ToCharArray())
+            {
+                uiText.text += letter;
+
+                if (PlayerPrefs.GetInt("Music") == 0 && sound && audioSource)
+                {
+                    audioSource.PlayOneShot(sound);
+                }
+
+                yield return new WaitForSeconds(letterPause);
+            }
+        }
+    }
+}
