ec8cac13ed9939ccf7b2af04817ad8f803400ceb
Hamza Langah <mh2094463@gmail.com>
2025-04-09 10:40:21 +0500

    2

---
+++ Assets/Scenes/SceneBundleLoader.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class SceneBundleLoader : MonoBehaviour
+{
+    string bundleURL = "https://raw.githubusercontent.com/hamzalangah/Bus-Simulator-Modern-Bus-Game/b7f9a3be8994bdac85772bcf1313ea7036f0cfcf/BuiltScenes/gameplay.bundle";
+
+    void Start()
+    {
+        StartCoroutine(DownloadAndLoadScene());
+    }
+
+    IEnumerator DownloadAndLoadScene()
+    {
+        Debug.Log("Downloading scene bundle...");
+        UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(bundleURL);
+        yield return request.SendWebRequest();
+
+#if UNITY_2020_1_OR_NEWER
+        if (request.result != UnityWebRequest.Result.Success)
+#else
+        if (request.isNetworkError || request.isHttpError)
+#endif
+        {
+            Debug.LogError("Download failed: " + request.error);
+            yield break;
+        }
+
+        AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(request);
+
+        string[] scenes = bundle.GetAllScenePaths();
+        Debug.Log("Scenes found in bundle: " + string.Join(", ", scenes));
+
+        if (scenes.Length > 0)
+        {
+            string scenePath = scenes[0];
+            string sceneName = Path.GetFileNameWithoutExtension(scenePath);
+            Debug.Log("Loading scene: " + sceneName);
+            SceneManager.LoadSceneAsync(sceneName);
+        }
+        else
+        {
+            Debug.LogError("No scenes found in the bundle!");
+        }
+    }
+}
