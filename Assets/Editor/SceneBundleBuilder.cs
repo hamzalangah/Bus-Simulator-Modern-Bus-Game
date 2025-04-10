0280b8982ad611ca2bce727f80a48a69c44995ee
Hamza Langah <mh2094463@gmail.com>
2025-04-10 14:37:59 +0500

    dfdf

--- Assets/Editor/SceneBundleBuilder.cs
+++ Assets/Editor/SceneBundleBuilder.cs
@@ -4,59 +4,28 @@ using System.IO;
 
 public class SceneBundleBuilder
 {
-    [MenuItem("Tools/Build Scene Bundle")]
-    static void BuildSceneBundle()
+    [MenuItem("Tools/Build Scene AssetBundle")]
+    static void BuildSceneAssetBundle()
     {
-        // Save bundle outside of Assets folder
-        string bundlePath = Path.Combine(Application.dataPath, "../BuiltScenes");
-        if (!Directory.Exists(bundlePath))
-            Directory.CreateDirectory(bundlePath);
+        string bundleFolder = Path.Combine(Application.dataPath, "../BuiltScenes");
+        if (!Directory.Exists(bundleFolder))
+            Directory.CreateDirectory(bundleFolder);
 
-        string[] scenePaths = { "Assets/Scenes/gameplay.unity" }; // Change to your actual scene path
+        string scenePath = "Assets/Scenes/gameplay.unity";
 
-        // Ensure all necessary shaders are included
-        IncludeShadersInBundle(scenePaths);
+        AssetBundleBuild bundleBuild = new AssetBundleBuild
+        {
+            assetBundleName = "gameplaybundle",
+            assetNames = new[] { scenePath }
+        };
 
-        string bundleOutput = Path.Combine(bundlePath, "gameplay.bundle");
-
-        BuildPipeline.BuildPlayer(
-            scenePaths,
-            bundleOutput,
-            BuildTarget.Android,
-            BuildOptions.BuildAdditionalStreamedScenes | BuildOptions.CompressWithLz4
+        BuildPipeline.BuildAssetBundles(
+            bundleFolder,
+            new AssetBundleBuild[] { bundleBuild },
+            BuildAssetBundleOptions.ChunkBasedCompression,
+            BuildTarget.Android
         );
 
-        Debug.Log("Scene bundle built at: " + bundleOutput);
-    }
-
-    static void IncludeShadersInBundle(string[] scenePaths)
-    {
-        // Go through each scene and find all the materials and shaders
-        foreach (string scenePath in scenePaths)
-        {
-            UnityEngine.SceneManagement.Scene scene = UnityEngine.SceneManagement.SceneManager.GetSceneByPath(scenePath);
-            if (!scene.isLoaded)
-                continue;
-
-            var rootGameObjects = scene.GetRootGameObjects();
-            foreach (var go in rootGameObjects)
-            {
-                Renderer[] renderers = go.GetComponentsInChildren<Renderer>();
-                foreach (var renderer in renderers)
-                {
-                    Material[] materials = renderer.sharedMaterials;
-                    foreach (var material in materials)
-                    {
-                        Shader shader = material.shader;
-                        string shaderPath = AssetDatabase.GetAssetPath(shader);
-                        // Make sure shaders are included
-                        if (!string.IsNullOrEmpty(shaderPath))
-                        {
-                            AssetDatabase.ImportAsset(shaderPath);
-                        }
-                    }
-                }
-            }
-        }
+        Debug.Log("Scene AssetBundle built at: " + bundleFolder);
     }
 }
